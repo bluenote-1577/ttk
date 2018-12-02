d9a1f37f3a5fe85a4b9dd88586844fef0fb28ac5
Ryan Cotsakis <ryancotsakis@gmail.com>
2018-12-01 17:13:23 -0800

    Starting PersistenceSimplification. Creates persistencediagram and topolologicalsimplification objects in base code

---
+++ core/base/persistenceSimplification/PersistenceSimplification.cpp
@@ -0,0 +1,16 @@
+#include                  <PersistenceSimplification.h>
+
+using namespace std;
+using namespace ttk;
+
+PersistenceSimplification::PersistenceSimplification(){
+
+  inputData_ = NULL;
+  outputData_ = NULL;
+  triangulation_ = NULL;
+}
+
+PersistenceSimplification::~PersistenceSimplification(){
+  
+}
+
